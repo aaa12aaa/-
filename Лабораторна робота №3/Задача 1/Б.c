e1a0b1d8488df9ff62f3b9933a3666fac8c10a92
aaa12aaa <79574455+aaa12aaa@users.noreply.github.com>
2024-04-29 11:53:08 +0300

    Update and rename Новий Текстовий документ.txt to Б.c

---
+++ Лабораторна робота №3/Задача 1/Б.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int b, c;
+    double a;
+
+    printf("Введіть значення b (1, 2, 3): ");
+    scanf("%d", &b);
+
+    printf("Введіть значення c: ");
+    scanf("%d", &c);
+
+    if (b != 1 && b != 2 && b != 3) {
+        printf("Неправильне значення b. Потрібно ввести 1, 2 або 3.\n");
+        return 1; // Повертаємо 1, щоб показати, що програма закінчилася з помилкою.
+    } else {
+      
+        if (b == 1) {
+            a = 4 * b - 9 * c;
+            printf("Коли b = 1, a = %.2f\n", a);
+        } 
+        else if (b == 2) {
+            
+          if (c <= 2) {
+                a = sqrt(2 - b * c);
+                printf("При b = 2 та c = %d, a = %.2f\n", c, a);
+            } else {
+                printf("Для b = 2, c повинно бути менше або рівне 2.\n");
+                return 1;
+            }
+        } 
+        else if (b == 3) {
+            
+          if (c == 0) {
+                printf("Для b = 3, c не може бути 0.\n");
+                return 1;
+            } 
+          else {
+                a = b / pow(c, 2);
+                printf("При b = 3 та c = %d, a = %.2f\n", c, a);
+            }
+        }
+    }
+
+    printf("\na = %.2f\n", a);
+
+    return 0;
+}
