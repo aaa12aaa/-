ba1a2ccf76b6817404e49f27e02848e98983e83f
aaa12aaa <79574455+aaa12aaa@users.noreply.github.com>
2024-04-29 11:16:05 +0300

    1.C

--- Лабораторна робота №3/Задача 1/1.C
+++ Лабораторна робота №3/Задача 1/1.C
@@ -19,16 +19,17 @@ int main() {
         a = 4 * b - 9 * c;
         printf("Коли b = 1, a = %.2f\n", a);
     }
-
-    if (b == 2) {
-        if (b * c <= 2) {
-            printf("\nДля b = 2, c повинно бути таким,\n щоб 2 - b * c було не від'ємним.\n");
-            return 1;
-        }
-        a = sqrt(2 - b * c) / (b / pow(c, 2));
-        printf("При b = 2 та c = %d, a = %.2f\n", c, a);
-    }
-
+ 
+  if (b == 2) {
+      if (c <= 2) {
+          a = sqrt(2 - b * c);
+          printf("При b = 2 та c = %d, a = %.2f\n", c, a);
+      } else {
+          printf("Для b = 2, c повинно бути менше або рівне 2.\n");
+          return 1;
+      }
+  }
+  
     if (b == 3 && c == 0) {
         printf("Для b = 3, c не може бути 0.\n");
         return 1;
